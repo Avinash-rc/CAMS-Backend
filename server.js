fd93afd0cfeba742bcf4fccd3499aa265316d4a7
Avinash Chavan <avinashrc2710@gmail.com>
2026-03-08 14:06:40 +0530

    added root route

--- server.js
+++ server.js
@@ -4,6 +4,10 @@ const authRoutes = require("./routes/auth");
 
 fastify.register(authRoutes, { prefix: "/auth" });
 
+fastify.get('/', async (request, reply) => {
+    return { message: "CAMS Backend Running" };
+});
+
 fastify.listen({port:3000,host:"0.0.0.0"}, (err) => {
 
     if (err) {
